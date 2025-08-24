7993e80af0cc6079bb506bbf984f5f1cad99ecec
daes10 <tech@sub.swissdeep.ch>
2025-08-24 02:04:58 +0200

    Delete wrong defaultvalue

--- src/eth02/Notendurchschnitt.java
+++ src/eth02/Notendurchschnitt.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Notendurchschnitt {
-    //    int defaultValue = 0;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float inputNote;
